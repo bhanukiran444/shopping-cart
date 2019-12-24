ce6736b1b5f1ea8e4714a66f04f1aa690fa2af9c
bhanu kiran <bhanukiran444@gmail.com>
2019-12-24 22:24:24 +0530

    changed firebase config file to mine

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyAxoFRN4wgFYX2DRYlOFb5kaspX20RzlXw',
+  authDomain: 'shopping-cart-eaf33.firebaseapp.com',
+  databaseURL: 'https://shopping-cart-eaf33.firebaseio.com',
+  projectId: 'shopping-cart-eaf33',
+  storageBucket: 'shopping-cart-eaf33.appspot.com',
+  messagingSenderId: '728160974517',
+  appId: '1:728160974517:web:671102ad5e25166054b0fc',
+  measurementId: 'G-ZWDQEDNDEQ'
 };
 
 firebase.initializeApp(config);
