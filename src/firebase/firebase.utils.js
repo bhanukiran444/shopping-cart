d0031c51d4ca37f81c97b9e256a4fbfefb5865a8
bhanu kiran <bhanukiran444@gmail.com>
2020-04-01 00:20:34 +0530

    collections data coming from firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,8 +18,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const colelctionRef = firestore.collection('users');
 
   const snapShot = await userRef.get();
+  const collectionSnapshot = await colelctionRef.get();
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -37,6 +39,38 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
